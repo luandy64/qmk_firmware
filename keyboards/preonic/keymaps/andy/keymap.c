859b0db83d25e6efc0447c9a34cd57dacab7f78c
Andy Lu <andy@stitchdata.com>
2018-12-31 17:00:43 -0600

    v2.1.0 Move volume control buttons to re-add a left shift

--- keyboards/preonic/keymaps/andy/keymap.c
+++ keyboards/preonic/keymaps/andy/keymap.c
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = {
     {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
-    {KC_VOLU, _______, _______, KC_LCBR, KC_RCBR, MY_UNDS, KC_PLUS, KC_7,    KC_8,    KC_9,    KC_SLSH, KC_NO  },
-    {KC_VOLD, _______, _______, KC_LBRC, KC_RBRC, KC_EQL , KC_MINS, KC_4,    KC_5,    KC_6,    KC_0   , KC_NO  },
-    {KC_MUTE, _______, _______, KC_LPRN, KC_RPRN, KC_BSLS, KC_ASTR, KC_1,    KC_2,    KC_3,    KC_DOT , _______},
+    {KC_MUTE, KC_VOLD, KC_VOLU, KC_LCBR, KC_RCBR, MY_UNDS, KC_PLUS, KC_7,    KC_8,    KC_9,    KC_SLSH, KC_NO  },
+    {_______, _______, _______, KC_LBRC, KC_RBRC, KC_EQL , KC_MINS, KC_4,    KC_5,    KC_6,    KC_0   , KC_NO  },
+    {_______, _______, _______, KC_LPRN, KC_RPRN, KC_BSLS, KC_ASTR, KC_1,    KC_2,    KC_3,    KC_DOT , _______},
     {_______, _______, _______, _______, _______, _______, _______, _______, CK_TOGG, CK_RST , CK_UP  , CK_DOWN}
   },                                     //HOLD//
 
