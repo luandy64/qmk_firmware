f1f9ede18a717486121945285c1af0eaed06e383
Andy Lu <andy@stitchdata.com>
2019-01-11 09:29:03 -0500

    WIP Cleaning up macros and aliases

--- keyboards/preonic/keymaps/andy/keymap.c
+++ keyboards/preonic/keymaps/andy/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = {
     {KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC},
     {KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_ENT },
-    {KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT},
+    {KC_LGUI, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT},
     {KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT},
-    {KC_ESC , KC_LCTL, KC_LALT, KC_LGUI, LOWER  , KC_SPC , KC_SPC , RAISE  , KC_RALT, KC_RGUI, KC_RCTL, KC_DEL }
+    {KC_ESC , KC_LCTL, KC_LALT, KC_LCTL, LOWER  , KC_SPC , KC_SPC , RAISE  , KC_RALT, KC_RGUI, KC_RCTL, KC_DEL }
   },
 
   [_LOWER] = {
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = {
     {KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 },
-    {_______, KC_NO  , KC_NO  , KC_NO  , EMC_VER, EMC_OTH, KC_NO  , KC_NO  , KC_UP  , KC_NO  , KC_NO  , _______},
-    {KC_ESC , KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO  , KC_NO  , KC_LEFT, KC_DOWN, KC_RGHT, KC_TAB , KC_DEL },
+    {_______, KC_NO  , KC_NO  , KC_NO  , EMC_VER, EMC_OTH, EMC_OUP, KC_NO  , KC_UP  , KC_NO  , KC_NO  , _______},
+    {_______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO  , EMC_ODN, KC_LEFT, KC_DOWN, KC_RGHT, KC_TAB , KC_DEL },
     {_______, TMUX_0 , TMUX_1 , TMUX_2 , TMUX_3 , TMUX_4 , KC_NO  , TMUX_LD, TMUX_SC, TMUX_LO, KC_NO  , _______},
-    {_______, _______, _______, _______, _______, _______, _______, _______, CK_TOGG, CK_RST , CK_UP  , CK_DOWN}
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
   },                                                                //HOLD//
 
 // MU_ON - Turn music mode on
