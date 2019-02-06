e36e75e68dd5e98040efeee884fc921e333fcbde
Andy Lu <luandy64@gmail.com>
2019-02-06 00:42:46 -0500

    Experiments with the gherkin

--- keyboards/40percentclub/gherkin/keymaps/default/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/default/keymap.c
@@ -5,9 +5,14 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ortho_3x10(
+    /*
     FN1_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ESC,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_BSPC, KC_SPC,  KC_B,    KC_N,    KC_M,    KC_ENT
+    */
+    KC_NO, KC_NO, KC_NO, KC_ACL2, KC_NO, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_NO,
+    RESET, KC_NO, KC_NO, KC_ACL1, KC_NO, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_ACL0, KC_NO, KC_WH_L, KC_WH_R, KC_NO  , KC_NO  , KC_NO
   ),
 
   [1] = LAYOUT_ortho_3x10(
